5154ad55ea83cef6a0eca0c1bd8cec4600703efe
404Fafnir <heikocarlier@gmail.com>
2023-02-02 11:48:25 +0100

    autoscroll ça fonctionne

--- scrap.py
+++ scrap.py
@@ -2,26 +2,33 @@ import requests
 from bs4 import BeautifulSoup as Soupe
 import re
 
-baseUrl = "https://lagrandefamilledesclowns.art/directory/index?profile_type=person"
 
-response = requests.get(baseUrl)
 
-if response.ok:
 
-    soupeOcailloux = Soupe(response.text, 'html.parser')
-    
-    ul = soupeOcailloux.find('div', {"id": "members_list"}) #pour chercher des classes particulières -->     ,{"class": ""} 
-    soc = ul.findAll('div',{"class": "result"})
+for nb in range(5):
 
-    
-    for li in soc:
+    baseUrl = ("https://lagrandefamilledesclowns.art/directory/index?page=" + str(nb) + "&profile_type=person")
 
-        a = li.find('a')
+    response = requests.get(baseUrl)
+
+    if response.ok:
+
+        soupeOcailloux = Soupe(response.text, 'html.parser')
         
-        try:
-            print(baseUrl + a['href'])
-        except:
-            pass
+        ul = soupeOcailloux.find('div', {"id": "members_list"}) #pour chercher des classes particulières -->     ,{"class": ""} 
+        soc = ul.findAll('div',{"class": "result"})
+
+        
+        for li in soc:
+
+            a = li.find('a')
+            
+            try:
+                
+                print(baseUrl + a['href'])
+            
+            except:
+                pass
                 
 print(response)
 
